29c2bc43b0cb06cc153eafc2ee0fad2fac0d2e09
anilkumarhp <hpanilkumar15@gmail.com>
2024-10-11 18:14:31 +0530

    added condition to check if sum is >, < or == product

--- math_script.sh
+++ math_script.sh
@@ -16,4 +16,15 @@ echo "The sum of $n1 and $n2 is $sum"
 echo "The product of $n1 and $n2 is $product."
 
 
+# check if sum is >, < or == to product.
+if [ $sum == $product ]
+then
+    echo "The sum is equal to the product."
+elif [ $sum > $product ]
+then
+    echo "The sum is greater than the product."
+elif [ $sum -lt $product ]
+then
+    echo "The sum is less than the product."
+fi
 
