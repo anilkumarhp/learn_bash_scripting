a3ea9d75deb6b3b984b27b9afdaed782a8298d08
anilkumarhp <hpanilkumar15@gmail.com>
2024-10-11 18:02:44 +0530

    added logic for performing addition and multiplication

--- math_script.sh
+++ math_script.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+
+# Request user to enter integer values.
+echo -n "Enter an integer: "
+read n1
+echo -n "Enter another integer: "
+read n2
+
+# perform addition operation.
+sum=$(($n1+$n2))
+
+# perform multiplication operation.
+product=$(($n1*$n2))
+
+echo "The sum of $n1 and $n2 is $sum"
+echo "The product of $n1 and $n2 is $product."
+
+
+
