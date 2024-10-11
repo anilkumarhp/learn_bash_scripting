0aec504efbe56264fd146d9e3ac6832fb9e43b3c
anilkumarhp <hpanilkumar15@gmail.com>
2024-10-11 19:24:49 +0530

    added logics for creating new array and populate the diffrence

--- csv_file_read.sh
+++ csv_file_read.sh
@@ -11,3 +11,17 @@ column_2=($(cut -d "," -f 3 $csv_file))
 # print first array
 echo "Displaying the first column:"
 echo "${column_0[@]}"
+
+## Create a new array as the difference of columns 1 and 2
+# initialize array with header
+column_3=("difference")
+
+# get the number of lines in each column
+nlines=$(cat $csv_file | wc -l)
+echo "There are $nlines lines in the file"
+
+# populate the array
+for ((i=1; i<$nlines; i++)); do
+  column_3[$i]=$((column_2[$i] - column_1[$i]))
+done
+echo "${column_3[@]}"
